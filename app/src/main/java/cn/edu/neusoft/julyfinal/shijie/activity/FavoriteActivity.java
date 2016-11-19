4e96f82d8fe8a3f557e9341cbd899b4c9bfcd573
julyfinal <julyfinal@vip.qq.com>
2016-11-19 11:32:27 +0800

    second:add some xml

---
+++ app/src/main/java/cn/edu/neusoft/julyfinal/shijie/activity/FavoriteActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.neusoft.julyfinal.shijie.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.edu.neusoft.julyfinal.shijie.R;
+
+public class FavoriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+    }
+}
